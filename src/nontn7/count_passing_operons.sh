ab33f2c66b7c5d79e2abd25d85b7f3b6977888f2
James Rybarski <rybarskj@wilkcomp01.ccbb.utexas.edu>
2020-11-30 23:41:48 -0600

    Add and update non-Tn7 scripts

--- src/nontn7/count_passing_operons.sh
+++ src/nontn7/count_passing_operons.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Counts how many operons passed all the rules for each branch of rules
+# Counts how many operons are in each gzipped file in a directory.
 
-for filename in $(fd csv.gz ../../output/nontn7); do 
+for filename in $(fd csv.gz ../../output/nontn7/$1); do 
     # Find all unique combinations of accession and contig coordinates
     count=$(gzip -cd $filename | awk 'BEGIN {FS=","};{print $1 $2}' | uniq | wc -l)
     echo "$count,$filename"
