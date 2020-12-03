bd5e7ecfccb3071cc5aa64acddd5842bd4a192d0
James Rybarski <rybarskj@wilkcomp01.ccbb.utexas.edu>
2020-12-03 13:26:19 -0600

    Add scripts to determine numbers for the paper

---
+++ src/nontn7/paper-values.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This script calculates several numerical values that we report in the text of the paper.
+
+OUTPUT="/stor/home/rybarskj/Metagenomics_CRISPR_transposons/output/nontn7"
+GROUPS="cas9 cas12 cas13 class1"
+# 1. The count of the number of systems we found that meet the basic rules.
+
+function count_operons () {
+    echo $(fd -a '.*csv.gz' $OUTPUT/$1.clusters | parallel -j2 'gzip -cd {}' | python count_operons.py)
+}
+
+cas9count=$(count_operons cas9)
+cas12count=$(count_operons cas12)
+cas13count=$(count_operons cas13)
+class1count=$(count_operons class1)
+
+printf "The pipeline identified %'d putative Cas9-encoding systems, %'d Cas12-encoding systems, %'d Cas13-encoding systems and %'d Type I systems.\n" $cas9count $cas12count $cas13count $class1count 
