2275062dc03cd5c23e4c76014305a15e4358e679
Kuang Hu <kh36969@wilkcomp01.ccbb.utexas.edu>
2020-12-23 16:17:46 -0600

    NCBI

--- analysis/NCBI/operon_analysis.py
+++ analysis/NCBI/operon_analysis.py
@@ -4,10 +4,10 @@ from operon_analyzer.rules import RuleSet, FilterSet
 
 
 
-rs = RuleSet().require('tnsB',regex=True).require('cas7',regex=True).require('cas6',regex=True).require('tnsA',regex=True).max_distance('tnsA', 'tnsB', 2000,closest_pair_only=True)
+rs = RuleSet().require('tnsB',regex=True).require('cas7',regex=True).require('cas6',regex=True).require('tnsA',regex=True).require('rsmJ').require('tniQ').max_distance('tnsA', 'tnsB', 2000,closest_pair_only=True)
 
 fs = FilterSet().pick_overlapping_features_by_bit_score(0.9).must_be_within_n_bp_of_anything(20000)
 
-with open('NCBI.csv', 'w', newline='') as csvfile:
-    with open('/stor/work/Wilke/crisprtn7/NCBI.csv', "r") as f:
+with open('NCBI2.csv', 'w', newline='') as csvfile:
+    with open('NCBI_genefinder2.csv', "r") as f:
         analyze(f, rs, fs,csvfile)
