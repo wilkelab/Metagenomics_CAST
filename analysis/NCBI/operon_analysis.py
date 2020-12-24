9727f2a3c19af2f938ff1bf3d3838e990fbe48fd
Kuang Hu <kh36969@wilkcomp01.ccbb.utexas.edu>
2020-12-23 21:04:56 -0600

    ncbi

--- analysis/NCBI/operon_analysis.py
+++ analysis/NCBI/operon_analysis.py
@@ -4,10 +4,10 @@ from operon_analyzer.rules import RuleSet, FilterSet
 
 
 
-rs = RuleSet().require('tnsB',regex=True).require('cas7',regex=True).require('cas6',regex=True).require('tnsA',regex=True).require('rsmJ').require('tniQ').max_distance('tnsA', 'tnsB', 2000,closest_pair_only=True)
+rs = RuleSet().require('tnsB',regex=True).require('cas7',regex=True).require('cas6',regex=True).require('tnsA',regex=True).require('glmS').require('tniQ').max_distance('tnsA', 'tnsB', 2000,closest_pair_only=True).max_distance('glmS', 'tniQ', 2000,closest_pair_only=True)
 
 fs = FilterSet().pick_overlapping_features_by_bit_score(0.9).must_be_within_n_bp_of_anything(20000)
 
-with open('NCBI2.csv', 'w', newline='') as csvfile:
-    with open('NCBI_genefinder2.csv', "r") as f:
+with open('NCBI.csv', 'w', newline='') as csvfile:
+    with open('NCBI_genefinder_merge.csv', "r") as f:
         analyze(f, rs, fs,csvfile)
