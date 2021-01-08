e5d124f245747b6f1cbe98eb15a694a889ccd90f
Kuang Hu <kh36969@wilkcomp01.ccbb.utexas.edu>
2021-01-07 22:47:11 -0600

    classII-finished

---
+++ analysis/typeII/rules-cas9.py
@@ -0,0 +1,14 @@
+import sys
+from operon_analyzer.analyze import analyze
+from operon_analyzer.rules import RuleSet, FilterSet
+
+
+input_genefinder_path,out_path = sys.argv[1:]
+
+rs = RuleSet().require('tnsB',regex=True).require('cas9',regex=True).require('tniQ').require('tnsC').max_distance('tnsC', 'tnsB', 2000,closest_pair_only=True)
+
+fs = FilterSet().pick_overlapping_features_by_bit_score(0.9).must_be_within_n_bp_of_anything(20000)
+
+with open(out_path, 'w', newline='') as csvfile:
+    with open(input_genefinder_path, "r") as f:
+        analyze(f, rs, fs,csvfile)
---
+++ analysis/typeII/test.sh
@@ -0,0 +1,5 @@
+
+RESULTS="./results"
+diagram_dir=$RESULTS/class2_diagrams_
+mkdir $diagram_dir 
+mkdir $diagram_dir/tn7-cas9
