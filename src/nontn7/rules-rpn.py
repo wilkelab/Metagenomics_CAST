e0dae8a4ca20e1c5407c1d737e89875d7e757f8b
Jim Rybarski <jim@rybarski.com>
2021-06-29 10:05:40 -0500

    Fix Rpn script (#19)
    * Finalize code for the non-Tn7 pipeline.
    
    * Fix bug in Rpn script and update data

--- src/nontn7/rules-rpn.py
+++ src/nontn7/rules-rpn.py
@@ -3,11 +3,21 @@
 
 import sys
 
-from operon_analyzer import analyze, rules
+from operon_analyzer import analyze, genes, rules
 
 from tools.filters import fs
 
+
+def _has_rpn_rule(operon: genes.Operon):
+    for feature in operon:
+        if feature.description.startswith('Rpn'):
+            return True
+    return False
+
+
+rpn_rule = rules.Rule('has-rpn', _has_rpn_rule)
+
 rs = rules.RuleSet().require('cas12', regex=True) \
-                    .require('^Rpn', regex=True)
+                    .custom(rpn_rule)
 
 analyze.evaluate_rules_and_reserialize(sys.stdin, rs, fs)
