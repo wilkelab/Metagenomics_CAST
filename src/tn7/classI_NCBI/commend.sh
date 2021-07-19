ed1d2983026f2189e1ce2cf9aa4b7fa101e0c4f5
Kuang Hu <kh36969@wilkcomp01.ccbb.utexas.edu>
2021-07-18 22:44:25 -0500

    Make sure the src folder only contain the script, and move the output to ouput folder

--- src/tn7/classI_NCBI/commend.sh
+++
@@ -1,3 +0,0 @@
-#main
-python gene_finder_att_site.py fa_database genefinder_result /stor/work/Wilke/blastDB/classifer/tns /stor/work/Wilke/blastDB/classifer/cas /stor/work/Wilke/blastDB/classifer/att /stor/work/Wilke/blastDB/classifer/ffs
-python gene_finder_att_site.py /stor/work/Wilke/crisprtn7/NCBI_reblast ./genefinder /stor/work/Wilke/blastDB/classifer/tns /stor/work/Wilke/blastDB/classifer/cas /stor/work/Wilke/blastDB/classifer/att /stor/home/kh36969/p_database/ffs/ffs
